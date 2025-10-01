c0674522d6d0b50e933b3f39f15bc34fea2ad165
TroelsAtWork <tras@kea.dk>
2025-10-01 11:28:23 +0200

    Update branch

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,4 @@
-import UseStateExercise from "./UseStateExercise";
-import LikeCounterExercise from "./LikeCounterExercise";
-import ThemeToggleStudentExercise from "./ThemeToggleStudentExercise";
 import "./globals.css";
 export default function Home() {
-  return <UseStateExercise />;
-  return <LikeCounterExercise />;
-  return <ThemeToggleStudentExercise />;
+  return <h1>Vælg en branche for at se relevante opgaver</h1>;
 }
